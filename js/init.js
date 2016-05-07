b1183232220a6154131f6d4be4703d564ff8c1e1
JP-x <jpadi004@gmail.com>
2016-05-07 16:20:57 -0700

    checkboxes now synced

--- js/init.js
+++ js/init.js
@@ -9,20 +9,70 @@
 })(jQuery); // end of jQuery name space
 
 
+	   	if($('#night_mode').is(':checked'))
+   	 	{
+   	 		//bod.className = "dark white-text";
+   	 		index_banner.className = "parallax-container";
+   	 		n_banner.src="img/name_banner_night.png" ;
+   	 		banner_img.src="img/banner_night.jpg";
+   	 	}
+   	 	else
+   	 	{
+   	 		//bod.className = "white black-text";
+   	 		index_banner.className = "parallax-container-day";
+   	 		n_banner.src="img/name_banner_day.png" ;
+   	 		banner_img.src="img/banner_test.jpg";
+   	 	}
+
+   	 	if($('#night_mode').is(':checked'))
+   	 		$('#night_mode2').prop('checked',true);
+   	 	else
+   	 		$('#night_mode2').prop('checked',false);
+
+   	 	if($('#night_mode2').is(':checked'))
+   	 		$('#night_mode').prop('checked',true);
+   	 	else
+   	 		$('#night_mode').prop('checked',false);
+
     $('#night_mode').click(function() {
     //alert("Checkbox state (method 2) = " + $('#night_mode').is(':checked'));
    	 	if($('#night_mode').is(':checked'))
    	 	{
-   	 		bod.className = "grey darken-3 white-text";
-   	 		index_banner.className = "parallax-container"
-   	 		n_banner.src="img/name_banner_night.png" 
-   	 		banner_img.src="img/banner_night.jpg"
+   	 		//bod.className = "dark white-text";
+   	 		index_banner.className = "parallax-container";
+   	 		n_banner.src="img/name_banner_night.png" ;
+   	 		banner_img.src="img/banner_night.jpg";
+   	 		//check other checkbox
+   	 		$('#night_mode2').prop('checked',true);
    	 	}
    	 	else
    	 	{
-   	 		bod.className = "white black-text";
-   	 		index_banner.className = "parallax-container-day"
-   	 		n_banner.src="img/name_banner_day.png" 
-   	 		banner_img.src="img/banner_test.jpg"
+   	 		//bod.className = "white black-text";
+   	 		index_banner.className = "parallax-container-day";
+   	 		n_banner.src="img/name_banner_day.png" ;
+   	 		banner_img.src="img/banner_test.jpg";
+   	 		$('#night_mode2').prop('checked',false);
+   	 	}
+	});
+
+
+    $('#night_mode2').click(function() {
+    //alert("Checkbox state (method 2) = " + $('#night_mode').is(':checked'));
+   	 	if($('#night_mode2').is(':checked'))
+   	 	{
+   	 		//bod.className = "dark white-text";
+   	 		index_banner.className = "parallax-container";
+   	 		n_banner.src="img/name_banner_night.png" ;
+   	 		banner_img.src="img/banner_night.jpg";
+   	 		$('#night_mode').prop('checked',true);
+   	 	}
+   	 	else
+   	 	{
+   	 		//bod.className = "white black-text";
+   	 		index_banner.className = "parallax-container-day";
+   	 		n_banner.src="img/name_banner_day.png" ;
+   	 		banner_img.src="img/banner_test.jpg";
+   	 		//check other checkbox
+   	 		$('#night_mode').prop('checked',false);
    	 	}
 	});
