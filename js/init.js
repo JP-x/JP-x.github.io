dcb00523c055c3b052f55481dbd3cc04b190bd4c
JP-x <jpadi004@gmail.com>
2016-05-26 03:37:14 -0700

    Changed nav on project pages. Updated most buttons to go to appropriate design documents and repos. / .

--- js/init.js
+++ js/init.js
@@ -4,6 +4,8 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     $('nav').pushpin({ top: $('nav').offset().top });
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
