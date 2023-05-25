c9c12f0b6de739933fd32ff3e9cd2cfa47449378
Jonathan Padilla <jpadi004@gmail.com>
2023-05-24 20:24:53 -0700

    Further styling day mode.

--- jp-x-react/src/components/project/projectitem.js
+++ jp-x-react/src/components/project/projectitem.js
@@ -17,7 +17,7 @@ export default function ProjectItem({ imageDirection, data }) {
                         <div className="row info-text text-teal">
                             <div className="d-flex flex-sm-row flex-column text-center-on-sm sm-pb-05em">
                                 <div>
-                                    <a className="text-teal outlineButton" href={data.demoUrl} target="_blank" rel="noreferrer" role="button"><span>VIEW DEMO</span></a>
+                                    <a className="outlineButton" href={data.demoUrl} target="_blank" rel="noreferrer" role="button"><span>VIEW DEMO</span></a>
                                 </div>
                                 <div className="d-none d-md-block">
                                     &nbsp;|&nbsp;
