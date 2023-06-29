e8730b32fde5c5ca2c2688c7647927384921fbca
Jonathan Padilla <jpadi004@gmail.com>
2023-06-29 00:30:12 -0700

    Adjust icon sizes. Added stroke to icons to reduce line thickness.

--- jp-x-react/src/components/displayicon.js
+++ jp-x-react/src/components/displayicon.js
@@ -1,20 +1,21 @@
-import CasinoIcon from '@mui/icons-material/Casino';
-import RequestQuoteIcon from '@mui/icons-material/RequestQuote';
-import BrushIcon from '@mui/icons-material/Brush';
-import WebIcon from '@mui/icons-material/Web';
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import CasinoIcon from '@mui/icons-material/CasinoOutlined';
+import RequestQuoteIcon from '@mui/icons-material/RequestQuoteOutlined';
+import BrushIcon from '@mui/icons-material/BrushOutlined';
+import WebIcon from '@mui/icons-material/WebOutlined';
+import AccountBoxIcon from '@mui/icons-material/AccountBoxOutlined';
 
 export default function DisplayIcon({iconName})
 {
-    let size = 250;
+    let size = 175;
     let iconComponent;
+
     switch(iconName)
     {
         case "RequestQuoteIcon" :
             iconComponent = <RequestQuoteIcon sx={{fontSize: size}} className="icon-wrapper"/>
             break;
         case "AccountBoxIcon":
-            iconComponent = <AccountBoxIcon sx={{fontSize: size}} className="icon-wrapper"/>
+            iconComponent = <AccountBoxIcon sx={{ fontSize: size}} className="icon-wrapper"/>
             break;
         case "BrushIcon":
             iconComponent = <BrushIcon sx={{fontSize: size}} className="icon-wrapper"/>
@@ -27,5 +28,9 @@ export default function DisplayIcon({iconName})
             break;
         default:
     }
-    return iconComponent;
+    return (
+        <>
+            {iconComponent}
+        </>
+        );
 }
